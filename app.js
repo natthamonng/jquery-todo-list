8c22029ddca59aa38df254b20930519e951d27ef
Natthamon <55828770+natthamonng@users.noreply.github.com>
2020-01-10 11:59:20 +0100

    Develop (#1)
    * deleteTodo and toggleTodo in progress
    
    * Refactoring: ES6.

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
+import { loadData, createTodoData, toggleTodo, deleteTodo, clearCompletedTodos } from "./dataSvc.js";
+import { updateView, getTodoElementId } from "./view.js";
+
 $(function() {
-    // Valeur par défaut
-    let todosData = [
-        // {
-        //     id: 0,
-        //     text: "eat",
-        //     completed: false
-        // }
-    ];
-
-    let $todoList = $('.todoList');//.html(localStorage.getItem('todoList'));
+    /**
+     * Variable declaration
+     */
     let $addTodo = $('#addTodo');
+    const $clearCompleted = $('.clearCompleted');
 
+    /**
+     * App entrypoint
+     */
     loadData();
     updateView();
-  
+
+    /**
+     * Event Listeners
+     */
     $addTodo.on('submit', function (e) {
         e.preventDefault();
         
@@ -24,101 +27,24 @@ $(function() {
             updateView();
         }
         
-        // La même sans variables
-        // $todoList.append(
-        //     $('<li>' + text + '</li>').addClass('todoItem')
-        // );
-
-        // Par texte directement
-        // $todoList.append('<li class="todoItem">' + text + '</li>').addClass('todoItem');
-
-        // Donne classe à la liste (ul)
-        // $todoList.append('<li>' + text + '</li>').addClass('todoItem');
-
         $('input:text').val('');
     });
     
-    function createTodoElement(todoData){
-        // On créé, puis on append
-        const $todo = $(`
-        <li class="todoItem" data-todo-id="${todoData.id}"> 
-            <input class="toggleTodo ${todoData.completed ? 'checked' : ''}" type="checkbox" ${todoData.completed ? 'checked="checked"' : ''}"/> 
-            <span>${todoData.text}</span> 
-            <button class="deleteTodo">X</button>
-        </li>`);
-        $todoList.append($todo);
-    }
-
-    function createTodoData(text){
-        const todoData = {
-            id: createID(),
-            text,
-            completed: false
-        };
-        todosData.push(todoData);
-
-        saveData();
-
-        return todoData;
-    }
-
-    // Fonction responsable de synchroniser la page html avec le tableau todoData
-    function updateView(){
-        $todoList.html('');
-
-        for(let todoData of todosData){
-            createTodoElement(todoData);
-        }
-    }
-
-    function saveData(){
-        // Save
-        localStorage.setItem('todoData', JSON.stringify(todosData));
-    }
-
-    function loadData(){
-        // Chargement des données
-        const maybeSavedData = localStorage.getItem('todoData');
-        if(maybeSavedData != null){
-            todosData = JSON.parse(maybeSavedData);
-        }
-    }
-
-    function createID(){
-        let newID = -1;
-        const usedIDs = todosData.map(data => data.id);
-
-        do {
-            newID = _createID();
-        } while (usedIDs.indexOf(newID) > -1);
-
-        return newID;
-    }
-    function _createID(){
-        return Math.floor(Math.random() * 1000);
-    }
-
     $(document).on('click', '.deleteTodo', function() {
-        const $todo = $(this).parents('.todoItem');
-        const id = $todo.attr('data-todo-id');
-        // $this.remove();
-        deleteTodo(id)
-        // localStorage.setItem('todoList', $('.todoList').html());
-    });
-
-    function deleteTodo(id) {
-        todosData = todosData.filter(todoData.id !== id);
-        saveData();
+        const id = getTodoElementId(this);
+        deleteTodo(id);
         updateView();
-    }
+    });
 
     $(document).on('change', '.toggleTodo', function() {
-        if ($(this).attr('checked')) {
-            $(this).removeAttr('checked').removeClass('checked');
-        } else {
-            $(this).attr('checked', 'checked').addClass('checked');
-        }
-        $(this).parent().toggleClass('completed');
-        localStorage.setItem('todoList', $('.todoList').html());
+        const id = getTodoElementId(this);
+        toggleTodo(id);
+        updateView();
     });
+
+    $clearCompleted.on('click', () => {
+        clearCompletedTodos();
+        updateView();
+    });
+
 });
---
+++ dataSvc.js
@@ -0,0 +1,75 @@
+// Valeur par défaut
+let todosData = [];
+
+export function getData(){
+    return todosData;
+}
+
+/**
+ * Data
+ */
+export function loadData() {
+    // Chargement des données
+    const storedData = localStorage.getItem('todoData');
+    if(storedData != null) {
+        todosData = JSON.parse(storedData);
+    }
+}
+
+export function saveData(){
+    // Save
+    localStorage.setItem('todoData', JSON.stringify(todosData));
+}
+
+export function createTodoData(text){
+    const todoData = {
+        id: createID(),
+        text,
+        completed: false
+    };
+
+    todosData.push(todoData);
+
+    saveData();
+
+    return todoData;
+}
+
+export function getIncompleteTodos() {
+    return todosData.filter(todoData => !todoData.completed);
+}
+
+export function deleteTodo(id) {
+    todosData = todosData.filter(todoData => todoData.id !== id);
+    saveData();
+    // updateView();
+}
+
+export function toggleTodo(id) {
+    const todoData = todosData.find(todoData => todoData.id === id)
+    todoData.completed = !todoData.completed
+    
+    saveData();
+    // updateView();
+}
+
+export function clearCompletedTodos() {
+    todosData = getIncompleteTodos();
+    saveData();
+    // updateView();
+}
+
+export function createID(){
+    let newID = -1;
+    const usedIDs = todosData.map(data => data.id);
+
+    do {
+        newID = _createID();
+    } while (usedIDs.indexOf(newID) > -1);
+
+    return newID;
+}
+
+function _createID(){
+    return Math.floor(Math.random() * 1000);
+}
